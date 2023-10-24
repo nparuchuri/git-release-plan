25122174005ec135e7fd09c80cbb1d72d15f1102
nparuchuri <nparuchuri@gmail.com>
2023-10-23 22:19:23 -0500

    initial version

---
+++ src/main/java/com/nparuchuri/gitrelease/MainController.java
@@ -0,0 +1,17 @@
+package com.nparuchuri.gitrelease;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MainController {
+	
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "Greetings from Spring Boot!";
+	}
+
+}
